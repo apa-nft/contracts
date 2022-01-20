f91e925dd9f992c950573a25e95e8aa295455961
sam shakespeare <sam@fibmatrix.com>
2022-01-20 11:59:05 -0500

    added forge tests

---
+++ apa-forge-test/src/test/ERC721Mock.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity  ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract ERC721Mock is ERC721 {
+    constructor(string memory name, string memory symbol)
+        ERC721(name, symbol)
+    {} // solhint-disable-line
+
+    function mint(uint256 tokenId) external {
+        _mint(msg.sender, tokenId);
+    }
+}
